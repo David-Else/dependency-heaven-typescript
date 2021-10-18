38bc9ab68ed494ef49855ceee1fedddfb1d54c58
David <12832280+David-Else@users.noreply.github.com>
2021-10-18 08:55:02 +0100

    Fix broken link

--- deps.ts
+++ deps.ts
@@ -2,9 +2,9 @@ export {
   assert,
   assertEquals,
   assertStrictEquals,
-  copySync,
 } from "https://deno.land/std@0.111.0/testing/asserts.ts";
 
+export { copySync } from "https://deno.land/std@0.111.0/fs/mod.ts";
 // https://github.com/evanw/esbuild/issues/802#issuecomment-819579154
 export * as esbuild from "https://deno.land/x/esbuild@v0.13.8/mod.js";
 // std@0.106.0 is final version to use old server code
